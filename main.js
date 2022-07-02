6a8fbbad4366c71fb0081679936e71444d4fe1f4
QurbanovRamiz <qramizq123@gmail.com>
2022-07-02 13:30:01 +0400

    update main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+// var n = 1233;
+// var first;
+// var second;
+// var thith;
+// var fourth;
+// var vardir = "var";
+// var yoxdur = "yoxdur";
+// if (n <= 9999 && n >= 1000) {
+//   first = n % 10;
+//   n = (n - (n % 10)) / 10;
+//   second = n % 10;
+//   n = (n - (n % 10)) / 10;
+//   thith = n % 10;
+//   n = (n - (n % 10)) / 10;
+//   fourth = n % 10;
+// }
+// if (
+//   first == second ||
+//   first == thith ||
+//   first == fourth ||
+//   second == thith ||
+//   second == fourth ||
+//   thith == fourth
+// ) {
+//   console.log(vardir);
+// } else if (
+//   first != second &&
+//   first != thith &&
+//   first != fourth &&
+//   second != thith &&
+//   second != fourth &&
+//   thith != fourth
+// ) {
+//   console.log(yoxdur);
+// }
+
+var n = 12;
