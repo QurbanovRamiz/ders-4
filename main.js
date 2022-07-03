8ea810dd1641e4dfd73750f6d3b049f6c2f963fe
QurbanovRamiz <qramizq123@gmail.com>
2022-07-03 19:08:01 +0400

    ruslan komek

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-// var n = 7371;
-// var first;
-// var second;
-// var thirth;
-// var fourth;
-// var vardir = "var";
-// var yoxdur = "yoxdur";
-// if (n <= 9999 && n >= 1000) {
-//   first = n % 10;
-//   n = (n - (n % 10)) / 10;
-//   second = n % 10;
-//   n = (n - (n % 10)) / 10;
-//   thirth = n % 10;
-//   n = (n - (n % 10)) / 10;
-//   fourth = n % 10;
-// }
-// if (
-//   first == second ||
-//   first == thirth ||
-//   first == fourth ||
-//   second == thirth ||
-//   second == fourth ||
-//   thirth == fourth
-// ) {
-//   console.log(vardir);
-// } else console.log(yoxdur);
+function isRepeatNum(n) {
+  let first, second, thirth, fourth;
 
+  if (n > 9999 || n <= 1000) {
+    console.error("olmaz!!!!");
+    return 0;
+  }
+
+  first = n % 10;
+  n = (n - (n % 10)) / 10;
+  second = n % 10;
+  n = (n - (n % 10)) / 10;
+  thirth = n % 10;
+  n = (n - (n % 10)) / 10;
+  fourth = n % 10;
+
+  if (
+    first == second ||
+    first == thirth ||
+    first == fourth ||
+    second == thirth ||
+    second == fourth ||
+    thirth == fourth
+  ) {
+    console.log("vardir");
+  } else console.log("yoxdur");
+}
+
+isRepeatNum(1224);
 //  if (
 //   first != second &&
 //   first != thirth &&
@@ -36,7 +38,7 @@
 //   console.log(yoxdur);
 // }
 
-// var n = 948765;
+// var n = 257369;
 // var reverseN = 0;
 // if (n > 0) {
 //   for (var num = n; num > 0; num = (num - (num % 10)) / 10) {
